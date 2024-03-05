9d7fac2d93df104005a4077257fe68e1dda534dd
Artem <vinogradovartem0412@gmail.com>
2024-03-06 00:48:23 +0300

    2day

---
+++ solutions_for_day_tasks/task7.py
@@ -0,0 +1,94 @@
+# import time                                                                                   #  
+# import math                                                                                   #
+# import rospy
+import keyboard                                                                                  # <- подгружаем нужные либы 
+# from std_msgs.msg import Bool, String, Odometry, Twist, UInt16, Int16, UInt8MultiArray        #
+# from tf.transformations import quaternion_multiply, quaternion_inverse, euler_from_quaternion #
+
+
+# def odom_callback(data): # получение одометрии
+#     global odom
+#     odom = data
+
+# def move_func(x,z): # публикуем в топик /cmd_vel переменные x,z соответсвенно для прямолинейного движения и поворота
+#     pub_vel = Twist()
+#     pub_vel.linear.x = x
+#     pub_vel.angular.z = z
+#     cmd_vel_pub.publish(pub_vel)
+
+# def get_distance(start_pose, current_pose): # фунция для возврата пройденного расстояния 
+#     return math.sqrt(math.pow(start_pose.x - current_pose.x, 2) + math.pow(start_pose.y - current_pose.y, 2))
+
+
+# def get_degree_diff(prev_orientation, current_orientation): # фунция для возврата угла, на который повернулся ровер 
+#         prev_q = [prev_orientation.pose.pose.orientation.x, prev_orientation.pose.pose.orientation.y, prev_orientation.pose.pose.orientation.z, prev_orientation.pose.pose.orientation.w]
+#         current_q = [current_orientation.pose.pose.orientation.x, current_orientation.pose.pose.orientation.y, current_orientation.pose.pose.orientation.z, current_orientation.pose.pose.orientation.w]
+#         delta_q = quaternion_multiply(prev_q, quaternion_inverse(current_q))
+#         (_, _, yaw) = euler_from_quaternion(delta_q)
+#         angle = math.degrees(yaw)
+        
+#         return angle
+
+# def move(dist, vel): # функция для движения ровера на dist метров вперед/назад(зависит от знака скорости) со скоростью vel
+#     global odom
+#     rospy.sleep(0.5)
+#     start_pose = odom
+#     current_distance = abs(get_distance(start_pose.pose.pose.position, odom.pose.pose.position))
+#     dist += current_distance
+#     while not rospy.is_shutdown() and current_distance <= dist:
+#         current_distance = abs(get_distance(start_pose.pose.pose.position, odom.pose.pose.position))
+#         print(current_distance,dist) 
+#         move_func(vel, 0) 
+#     rospy.sleep(0.1)
+#     move_func(0, 0) 
+#     rospy.loginfo("goal achived") 
+#     rospy.sleep(0.1)
+
+# def rotate(angle, vel): # функция для повората ровера на angle градусов со скорость vel (направление поворота зависит от знака скорости)
+#     global odom
+#     rospy.sleep(0.5)
+#     start_orientation = odom
+#     current_angle = abs(get_degree_diff(start_orientation, odom))
+#     while not rospy.is_shutdown() and current_angle < angle:
+#         current_angle = abs(get_degree_diff(start_orientation, odom)) 
+#         print(current_angle, angle)
+#         z=vel
+#         move_func(0, z)
+#     move_func(0,0)
+#     rospy.loginfo('goal of the angle achived')
+#     rospy.sleep(0.1)
+
+
+if __name__ == "__main__":
+    # rospy.init_node('mover', log_level=rospy.INFO)
+    # rospy.Subscriber("odom", Odometry, odom_callback)
+    # cmd_vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    
+    # rospy.loginfo("success main init")
+    
+    # move(1.1, 0.5)
+    # rotate(180, 0.5)
+    # move(1.1, 0.5)
+    # move(-0.5, 0.5)
+    vel = 1
+    while True:
+        if keyboard.is_pressed(119):
+            print('moving forward with {vel}m/s speed')
+            x=vel
+        elif keyboard.is_pressed(115):
+            print('moving backward with {vel}m/s speed')
+            x=-vel
+        elif keyboard.is_pressed(100):
+            print('rotating right with {vel}m/s speed')
+            z=-vel
+        elif keyboard.is_pressed(97):
+            print('rotating left with {vel}m/s speed')
+            z=vel
+        elif keyboard.is_pressed(61):
+            print('making faster (vel = {vel})')
+            vel+=0.1
+        elif keyboard.is_pressed(45):
+            print('making slower (vel = {vel})')
+            vel-=0.1
+        
+        
---
+++ solutions_for_day_tasks/task7_2.py
@@ -0,0 +1,82 @@
+import time                                                                                   #  
+import math                                                                                   #
+import rospy                                                                         # <- подгружаем нужные либы 
+from std_msgs.msg import Bool, String, Odometry, Twist, UInt16, Int16, UInt8MultiArray        #
+from tf.transformations import quaternion_multiply, quaternion_inverse, euler_from_quaternion #
+
+
+def odom_callback(data): # получение одометрии
+    global odom
+    odom = data
+
+def move_func(x,z): # публикуем в топик /cmd_vel переменные x,z соответсвенно для прямолинейного движения и поворота
+    pub_vel = Twist()
+    pub_vel.linear.x = x
+    pub_vel.angular.z = z
+    cmd_vel_pub.publish(pub_vel)
+
+def get_distance(start_pose, current_pose): # фунция для возврата пройденного расстояния 
+    return math.sqrt(math.pow(start_pose.x - current_pose.x, 2) + math.pow(start_pose.y - current_pose.y, 2))
+
+
+def get_degree_diff(prev_orientation, current_orientation): # фунция для возврата угла, на который повернулся ровер 
+        prev_q = [prev_orientation.pose.pose.orientation.x, prev_orientation.pose.pose.orientation.y, prev_orientation.pose.pose.orientation.z, prev_orientation.pose.pose.orientation.w]
+        current_q = [current_orientation.pose.pose.orientation.x, current_orientation.pose.pose.orientation.y, current_orientation.pose.pose.orientation.z, current_orientation.pose.pose.orientation.w]
+        delta_q = quaternion_multiply(prev_q, quaternion_inverse(current_q))
+        (_, _, yaw) = euler_from_quaternion(delta_q)
+        angle = math.degrees(yaw)
+        
+        return angle
+
+def move(dist, vel): # функция для движения ровера на dist метров вперед/назад(зависит от знака скорости) со скоростью vel
+    global odom
+    rospy.sleep(0.5)
+    start_pose = odom
+    current_distance = abs(get_distance(start_pose.pose.pose.position, odom.pose.pose.position))
+    dist += current_distance
+    while not rospy.is_shutdown() and current_distance <= dist:
+        current_distance = abs(get_distance(start_pose.pose.pose.position, odom.pose.pose.position))
+        print(current_distance,dist) 
+        move_func(vel, 0) 
+    rospy.sleep(0.1)
+    move_func(0, 0) 
+    rospy.loginfo("goal achived") 
+    rospy.sleep(0.1)
+
+def rotate(angle, vel): # функция для повората ровера на angle градусов со скорость vel (направление поворота зависит от знака скорости)
+    global odom
+    rospy.sleep(0.5)
+    start_orientation = odom
+    current_angle = abs(get_degree_diff(start_orientation, odom))
+    while not rospy.is_shutdown() and current_angle < angle:
+        current_angle = abs(get_degree_diff(start_orientation, odom)) 
+        print(current_angle, angle)
+        z=vel
+        move_func(0, z)
+    move_func(0,0)
+    rospy.loginfo('goal of the angle achived')
+    rospy.sleep(0.1)
+
+
+if __name__ == "__main__":
+    rospy.init_node('mover', log_level=rospy.INFO)
+    rospy.Subscriber("odom", Odometry, odom_callback)
+    cmd_vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    
+    rospy.loginfo("success main init")
+    
+
+    vel = 1
+    print(f"Starting rover, vel={vel} ")
+    while not rospy.is_shutdown():
+        command = str(input('Введите команду: '))
+        param, value = command.split()
+
+        if command == 'W':
+            move(value, vel)
+        elif command == 'R':
+            rotate(value, vel)
+        elif command == 'P':
+            make_photo()
+        elif command == 'V':
+            vel = param
